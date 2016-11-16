3f1c4185052f5291432a8225b52df918e902f27a
sharath-thirunagaru <sharath@Sharaths-MacBook-Pro.local>
2016-11-15 22:15:55 -0600

    Initial Version
    Just had some vague layout

---
+++ MatchingProcess.py
@@ -0,0 +1,67 @@
+#Read a file and write tokens into a new file for the required columns given
+
+import sys
+
+
+def fileparser(filepath,testlines):
+    f = open(filepath)
+    titles=[]
+    for i in range(testlines):
+        line =f.readline()
+        titles.append(line.split('|')[0])
+
+    return titles
+
+def tokenizer(iplist):
+
+    tokenlist =[]
+    for str in iplist:
+        strsplitlist = str.lower().split()
+        for token in strsplitlist:
+            tokenlist.append(token)
+            tf = strsplitlist.count(token)
+            normtf = float(tf/len(strsplitlist))
+            tfidf=calculatetfidf(token,normtf,iplist)
+
+
+
+
+
+
+def calculatetfidf(token,normtf,doclist):
+    doccount = len(doclist)
+    tokendoccount=0
+
+    for doc in doclist:
+        if token in doc.lower().split():
+            tokendoccount +=1
+    idf = float(doccount/tokendoccount)
+
+    return float(normtf*idf)
+
+
+
+
+
+
+def main():
+    args = sys.argv
+    reffile =args[1]
+    sourcefile =args[2]
+    testlines = args[3]
+
+    print 'Ref file : ',reffile
+    print 'Source file: ',sourcefile
+
+    reftitles = fileparser(reffile,int(testlines))
+    srctitles = fileparser(sourcefile,int(testlines))
+
+    print reftitles
+    print srctitles
+
+
+
+
+
+if __name__=='__main__':
+    main()
